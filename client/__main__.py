2f15145b68a372daf0bd59423e7db3d1508cd091
D. Doffy <cuongnsm@gmail.com>
2024-08-19 08:18:25 -0600

    convert chunk using udp for faster speed

--- client/__main__.py
+++ client/__main__.py
@@ -6,15 +6,26 @@ import numpy as np
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(('0.0.0.0', 9999))
 
+buffer = b''
+
 while True:
     # Receive the data
     data, _ = sock.recvfrom(65535)  # Adjust the buffer size as needed
+    buffer += data
+
+    try:
+        img = cv2.imdecode(np.frombuffer(buffer, np.uint8), cv2.IMREAD_COLOR)
+        if img is not None:
+            cv2.imshow('Stream', img)
+            buffer = b''
+    except cv2.error:
+        pass
 
     # Decode the JPEG image
-    img = cv2.imdecode(np.frombuffer(data, np.uint8), cv2.IMREAD_COLOR)
+    # img = cv2.imdecode(np.frombuffer(data, np.uint8), cv2.IMREAD_COLOR)
 
     # Display the image
-    cv2.imshow('Stream', img)
+    # cv2.imshow('Stream', img)
 
     # Break the loop on 'q' key press
     if cv2.waitKey(1) & 0xFF == ord('q'):
--- server/__main__.py
+++ server/__main__.py
@@ -8,7 +8,8 @@ width, height = 1920, 1080  # Adjust based on your screen resolution
 
 # Set up socket connection
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_address = ('<RECEIVER_IP>', 9999)
+server_address = ('192.168.100.14', 9999)
+CHUNK_SIZE = 9000  # Adjust the buffer
 
 with mss.mss() as sct:
     monitor = {"top": 0, "left": 0, "width": width, "height": height}
@@ -20,5 +21,14 @@ with mss.mss() as sct:
         # Convert the image to JPEG to reduce size
         _, jpeg_img = cv2.imencode('.jpg', img)
 
+        img_bytes = jpeg_img.tobytes()
+
+        img_length = len(img_bytes)
+        # Send the image in CHUNK_SIZE img_bytes over the socket
+        for i in range(0, img_length, CHUNK_SIZE):
+            print('send chunk', i)
+            chunk = img_bytes[i:i + CHUNK_SIZE]
+            sock.sendto(chunk, server_address)
+
         # Send the JPEG-encoded image over the socket
-        sock.sendto(jpeg_img.tobytes(), server_address)
+        # sock.sendto(jpeg_img.tobytes(), server_address)
